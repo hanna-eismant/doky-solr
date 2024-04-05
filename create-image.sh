b3988104ca123d732b4b282760c476e8e0066e85
Hanna Kurhuzenkava <hanna_kurhuzenkava@epam.com>
2024-04-05 14:36:22 +0300

    MAIN-T-93 Configure Solr on Azure

--- create-image.sh
+++ create-image.sh
@@ -1,4 +1,4 @@
 #tar -r doky-solr.tgz ./solr/*
-cd solr || exit
-tar -cvzf doky-solr.tgz ./
-docker build -f docker/Dockerfile -t doky-solr:latest - < doky-solr.tgz
+#cd solr || exit
+tar -cvzf doky-solr.tgz solr-9.5.0
+DOCKER_BUILDKIT=0 docker build -f solr-9.5.0/docker/Dockerfile -t doky-solr:latest - < doky-solr.tgz
